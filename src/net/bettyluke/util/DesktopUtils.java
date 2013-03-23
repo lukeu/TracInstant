d3967d0f6321a78582d54b7a00fd24b4fed27f14
Luke Usherwood <l.usherwood@psenterprise.com>
2013-03-23 23:49:56 +0100

    Move method out to a utility class

---
+++ src/net/bettyluke/util/DesktopUtils.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2011 Luke Usherwood.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.bettyluke.util;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public final class DesktopUtils {
+    private DesktopUtils() {}
+
+    public static void browseTo(URL url) {
+        if (url == null) {
+            return;
+        }
+        try {
+            // Note, this can end up throwing UnsupportedOperationException
+            // even if we use the various "is supported" checks provided
+            // by Desktop.
+            Desktop.getDesktop().browse(url.toURI());
+        } catch (UnsupportedOperationException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (URISyntaxException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
