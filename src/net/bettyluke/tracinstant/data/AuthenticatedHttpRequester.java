2235f0da3f50a76e2d2c18ff49a34809a66ddc15
Luke Usherwood <ldubox-coding101@yahoo.co.uk>
2016-07-20 22:43:52 +0100

    Some more hackery to get https links to work

--- src/net/bettyluke/tracinstant/data/AuthenticatedHttpRequester.java
+++ src/net/bettyluke/tracinstant/data/AuthenticatedHttpRequester.java
@@ -4,10 +4,65 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import net.bettyluke.tracinstant.prefs.SiteSettings;
 
 public final class AuthenticatedHttpRequester {
+
+    // Some totally INSECURE code from SO to to make HTTPS links work...
+    //
+    //     http://stackoverflow.com/a/2793153/932359
+    //
+    // TODO: Security & certificates are not my area. Can anyone improve on this??
+    //
+    static {
+
+        TrustManager[] trustAllCertificates = new TrustManager[] {
+            new X509TrustManager() {
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null; // Not relevant.
+                }
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    // Do nothing. Just allow them all.
+                }
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    // Do nothing. Just allow them all.
+                }
+            }
+        };
+
+        HostnameVerifier trustAllHostnames = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true; // Just allow them all.
+            }
+        };
+
+        try {
+            System.setProperty("jsse.enableSNIExtension", "false");
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCertificates, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(trustAllHostnames);
+        }
+        catch (GeneralSecurityException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
     private AuthenticatedHttpRequester() {}
 
     public static InputStream getInputStream(SiteSettings settings, URL url) throws IOException {
