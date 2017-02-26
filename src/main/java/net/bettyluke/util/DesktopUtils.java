34da84aef496f491dbc2272d6002d31a4c2ab1b5
Luke Usherwood <ldubox-coding101@yahoo.co.uk>
2017-02-26 00:43:22 +0000

    Attempt to launch a URL on KDE

--- src/main/java/net/bettyluke/util/DesktopUtils.java
+++ src/main/java/net/bettyluke/util/DesktopUtils.java
@@ -20,6 +20,7 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Arrays;
 
 public final class DesktopUtils {
     private DesktopUtils() {}
@@ -33,8 +34,37 @@ public final class DesktopUtils {
             // even if we use the various "is supported" checks provided
             // by Desktop.
             Desktop.getDesktop().browse(url.toURI());
-        } catch (UnsupportedOperationException | IOException | URISyntaxException ex) {
+        } catch (UnsupportedOperationException ex) {
+            browseToPlanB(url);
+        } catch (IOException | URISyntaxException ex) {
             ex.printStackTrace();
         }
+    }
+
+    private static void browseToPlanB(URL url) {
+        for (String command : Arrays.asList("kde-open", "open")) {
+            if (browseToPlanB(command, url)) {
+                break;
+            }
+        }
+    }
+
+    private static boolean browseToPlanB(String command, URL url) {
+        String[] cmdarray = { '"' + command + '"', '"' + url.toString() + '"' };
+        try {
+            Process process = Runtime.getRuntime().exec(cmdarray);
+            if (process != null) {
+                try {
+                    // NB: Getting an IllegalThreadStateException means the process is still running
+                    process.exitValue();
+                } catch (IllegalThreadStateException ex) {
+                    return true;
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            // Ignore
+        }
+        return false;
     }
 }
